924228a7fda11679a6c7c9f8fafc8b5471f8113f
Kelly Becker & Natalie Blackburn <student@epicodus.com>
2014-03-03 14:57:17 -0800

    adds support for excluded words and special characters

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,9 +1,18 @@
 def title_case(input)
+  input = input.gsub(/[^A-Za-z\s]/, '')
   words = input.downcase.split
+  first_word = words.shift
   output = []
+  exceptions = ["the", "of", "and", "or", "nor", "a"]
+
+  first_word = first_word[0].upcase + first_word[1..-1]
+  output << first_word
+
   words.each do |x|
+    unless exceptions.include? x
       x = x[0].upcase + x[1..-1]
-      output << x
+    end
+    output << x
   end
   output.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -13,4 +13,16 @@ describe('title_case') do
   it('will return each word of the input with only the first letter of the word capitalized') do
     title_case("ruBy ROCKS").should(eq("Ruby Rocks"))
   end  
+
+  it('will return certain special characters uncaptialized') do
+    title_case("ruBy ROCKS tHE woRLd").should(eq("Ruby Rocks the World"))
+  end
+
+  it('will capitalize the first word, including exceptions') do
+    title_case("tHe grEAtest ProGRammers of RuBY").should(eq("The Greatest Programmers of Ruby"))
+  end
+
+  it('will remove anything that is not a letter or space') do 
+    title_case("tH54e grE%Atest Pr#{}oGRammers of RuBY!").should(eq("The Greatest Programmers of Ruby"))
+  end    
 end
